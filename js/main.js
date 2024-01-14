eecdce9b9ce3d5eb0752d74243002fee49926aef
Hasan Maghraby <maghrabyhasan.gmail.com>
2024-01-14 21:00:04 +0200

    main page

--- js/main.js
+++ js/main.js
@@ -204,6 +204,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
